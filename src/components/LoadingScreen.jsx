b550cf4afb5e5cc3df7c10cfde17a9028a50a5b5
Oni Ismail Modupe <ismailoni80@gmail.com>
2025-02-21 00:05:44 +0100

    final commit

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -6,8 +6,8 @@ export default function LoadingScreen() {
       <motion.h1
         initial={{ scale: 0, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 animate-ping delay-1000 tracking-wide"
+        transition={{ duration: 2 }}
+        className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 tracking-wide"
       >
         text.ai
       </motion.h1>
